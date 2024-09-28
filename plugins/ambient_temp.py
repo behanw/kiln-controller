83ca122afa6bcde000ceb441144aacb32b7ef806
Behan Webster <behanw@gmail.com>
2024-09-28 16:16:33 -0700

    Write OneWire Linux support for ambient temp sensors
    Since the Adafruit 1-wire circuitpython library doesn't support 1-wire
    on Linux, just use the /sys/bus/w1 interface which linux provides.
    
    Signed-off-by: Behan Webster <behanw@gmail.com>

--- plugins/ambient_temp.py
+++ plugins/ambient_temp.py
@@ -1,37 +1,63 @@
-import threading
-import time
 import logging
 import config
-
-from adafruit_onewire.bus import OneWireBus
-from adafruit_ds18x20 import DS18X20
+import os
+import glob
+import time
 
 log = logging.getLogger(__name__)
 
-import plugins
+#from lib.temptracker import TempTracker
+from plugins.kilnplugin import KilnPlugin
 
-class AmbientTemp(threading.Thread):
-    '''This represents a GPIO output that controls a
-    status LED which beats like a heart.
-        config.ambient_temp_gpio
+class OneWire(object):
+    bus = "/sys/bus/w1/devices/"
+
+    def __init__(self):
+        # Load the kernel drivers:
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
+
+    def scan(self):
+        return glob.glob(self.bus + '28*')
+
+class DS18X20(object):
+    def __init__(self, bus, device):
+        self.bus = bus
+        self.device = device
+        self.address = device.replace(bus.bus, '')
+        self.label = config.w1_ds18x20_label[self.address]
+        self.adjustment = config.w1_ds18x20_adjustment[self.address]
+
+    def temperature(self):
+        f = open(self.device+"/temperature", 'r')
+        temp = int(f.readlines()[0])
+        f.close
+        return temp / 1000 + self.adjustment
+
+class AmbientTemp(KilnPlugin):
+    '''This reads ambient temperature from one or more
+    DS18B20 sensors.
+        config.w1_ds18x20_label
+        config.w1_ds18x20_adjustment
         config.ambient_temp_quiet
     '''
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.daemon = True
+
+    sensors = {}
+
+    def __init__(self, hook=None):
+        super().__init__(hook)
 
         # Read AmbientTemp GPIO
-        try:
-            self.w1bus = OneWireBus(config.ambient_temp_gpio)
-            self.w1devs = self.w1bus.scan()
-            self.sensors = {}
-            for device in self.w1devs:
-                sensor = DS18X20(self.w1bus, device)
-                log.info( { 'name': sensor.address,
-                            'device': sensor } )
-            self.simulated = False
-        except:
-            self.simulated = True
+        #try:
+        self.w1bus = OneWire()
+        self.w1devs = self.w1bus.scan()
+        for device in self.w1devs:
+            sensor = DS18X20(self.w1bus, device)
+            name = sensor.address
+            self.sensors[name] = sensor
+        self.simulated = False
+        #except:
+        #    self.simulated = True
 
         # Quiet AmbientTemp during simulation for debugging
         try:
@@ -41,11 +67,21 @@ class AmbientTemp(threading.Thread):
         if self.simulated and self.quiet:
             log.warn("AmbientTemp disabled during simulation")
 
-        self.start()
-
     # This method will be executed when the thread starts
     def run(self):
-        log.info("Starting AmbientTemp")
+        log.info(self.message("Starting AmbientTemp"))
 
         while True:
-            time.sleep(1)
+            for name, sensor in self.sensors.items():
+                if not self.quiet:
+                    log.info("{:12} {:8.1f}C  {}".format(sensor.label, \
+                        sensor.temperature(), name))
+            time.sleep(self.period)
+
+ambientobj = None
+
+def startPlugin(hook=None):
+    global ambientobj
+    ambientobj = AmbientTemp(hook)
+    ambientobj.start()
+    return ambientobj
