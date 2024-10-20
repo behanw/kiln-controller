c5c3d6c40c3b336f54b6550c657ed839dbccdf54
Behan Webster <behanw@gmail.com>
2024-10-20 16:42:41 -0700

    Refactor hookspecs
    Hook definitions are still changing as the control plane is redesigned
    
    Signed-off-by: Behan Webster <behanw@gmail.com>

--- plugins/hookspecs.py
+++ plugins/hookspecs.py
@@ -3,60 +3,88 @@ from . import hookspec
 class PluginHooks:
 
     @hookspec
+    def start_plugin(self):
+        """Start plugins. Used by all plugins
+        """
+        # kilnapp -> all plugins
+
+    @hookspec
+    def on_start(self):
+        """Run when app starts. (Not currently used)
+        """
+        # Not currently used
+        # kilnapp -> Whomever
+
+    @hookspec
     def activity(self):
         """Indicate that activity has ocurred. Used for heartbeat.
+           (Probably should use heat() instead?)
         """
+        # Deprecated?
+        # oven -> heartbeat
 
     @hookspec
-    def heat(self):
-        """Get time from a centralized source.
-        Time could be accelerated during simulation and testing.
-        """
-
-    @hookspec
-    def get_time():
-        """Get time from a centralized source.
-        Time could be accelerated during simulation and testing.
-        """
-
-    @hookspec
-    def start_firing():
+    def start_firing(self):
         """Start the firing.
+        Don't know it this is needed?
+        Perhaps a generic event hook?
         """
+        # Not currently used
+        # ? oven -> Controller
+        # Run profile?
 
     @hookspec
-    def abort_firing():
-        """Stop the firing due to error.
+    def get_time(self):
+        """Get time from a centralized source.
+        Time could be accelerated during simulation and testing.
+        Should be provided by the Oven (real or simulateds)
         """
+        # Not currently used
+        # ? -> Thermocouple?
 
     @hookspec
-    def record_meta(info: dict):
-        """Send data to UI. Key/Value is added to kiln.ovenState.
-        """
-
-    @hookspec
-    def record_temperature(info: dict):
+    def temperature_reading(self, info: dict):
         """Save Thermocouple temperate and heating rate.
+        Perhaps should be renamed?
+        Sends temperature readings to PID and UI.
         """
 
     @hookspec
-    def failure(info: dict):
+    def sensor_reading(self, info: dict):
+        """Thermocouple readings.  Sends sensor readings to PID and UI.
+        """
+
+    @hookspec
+    def heat(self, info):
+        """Turn heat on for a percentage of the duty cycle.
+        Controls relays, or simulated heat in the oven or kiln.
+        """
+        # Used by control to communicate to heater relay
+
+    @hookspec
+    def add_cost(self, info):
+        """Add cost for duty_cycle.
+        Heat leads to cost, which is sent to status.
+        """
+        # Heater -> State
+
+    @hookspec
+    def abort_firing(self):
+        """Stop the firing due to error.
+        Perhaps a generic event hook?
+        """
+        # Stop firing
+
+    @hookspec
+    def failure(self, info: dict):
         """Indicate a failure has ocurred.
         :param reason: failure reason
         """
+        # Used by caution
 
     @hookspec
-    def clear_failure(info: dict=None):
+    def clear_failure(self, info: dict=None):
         """Indicate the failure has been cleared.
         :param reason: Reason for failure being cleared
         """
-
-    @hookspec
-    def start_plugin():
-        """Start plugins
-        """
-
-    @hookspec
-    def on_start():
-        """Run when app starts
-        """
+        # Used by caution
